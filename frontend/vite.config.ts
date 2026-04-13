398d5c15dd12cc5acd082d5086538d8a2f0fa66a
h5rsh <hariom1203@gmail.com>
2026-04-14 00:18:58 +0530

    chore: setup github pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/h5rsh-portfolio/",
   server: {
     host: "::",
     port: 8080,
